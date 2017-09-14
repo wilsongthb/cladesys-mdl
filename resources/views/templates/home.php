e1d5e205549a702b434c2c70e9ebc6af587c8bb7
Wilson Pilco Nuñez <wilsonaux1@gmail.com>
2017-09-14 00:48:58 -0500

    funcional en ubuhntu 16.04

--- resources/views/templates/home.php
+++ resources/views/templates/home.php
@@ -1,4 +1,4 @@
-<div class="row">
+<!-- <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
         <h1 class="text-center">BIENVENIDO AL SISTEMA DE LOGISTICA</h1>
     </div>
@@ -8,7 +8,35 @@
     <div class="col-xs-12 col-sm-2 col-md-2 col-lg-3">
     </div>
     <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
-        <!-- <location-select></location-select> -->
-        <!-- <locations-crud></locations-crud> -->
+        <location-select></location-select>
+        <locations-crud></locations-crud>
+    </div>
+</div> -->
+
+
+<div class="row">
+    
+    <div class="col-sm-2 col-md-2 col-lg-2">
+        
+    </div>
+    
+    <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+
+        <div class="panel panel-default">
+            <div class="panel-body">
+
+                <h1>LATIGAZO - LOGISTICS</h1>
+                <h2>
+                    <p>Bienvenido al Sistema de Logistica, este <code>Sistema de Información</code> sirve para registrar productos,
+                        controlar la entrada de estos al almacén, seguir su distribución, controlar el stock de los productos
+                        </p>
+                    <p>- Control de Usuarios (pendiente)</p>
+                    <p>- Control de Diferentes ubicaciones </p>
+                    <p>- Proceso de compra, requerimientos, cotización y compra</p>
+                </h2>
+
+            </div>
+        </div>
+
     </div>
 </div>

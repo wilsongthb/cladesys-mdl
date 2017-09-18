398bfc5f1cabfd7cf9f3347b01d9a9d2858f841a
Wilson Pilco Nuñez <wilsonaux1@gmail.com>
2017-09-18 23:35:14 +0000

    agregado normalize css a logistic

--- resources/views/logistic/gentelella/index.blade.php
+++ resources/views/logistic/gentelella/index.blade.php
@@ -10,33 +10,27 @@
 
     <title>LOGISTICA</title>
 
+    {{--  normalize-css  --}}
+    <link rel="stylesheet" href="{{ asset('/bower_components/normalize-css/normalize.css') }} ">
     <!-- Bootstrap -->
     <link href="{{ asset('/bower_components/gentelella/vendors/bootstrap/dist/css/bootstrap.min.css') }} " rel="stylesheet">
     <!-- Font Awesome -->
     <link href="{{ asset('/bower_components/gentelella/vendors/font-awesome/css/font-awesome.min.css') }} " rel="stylesheet">
     <!-- NProgress -->
-    <link href="{{ asset('/bower_components/gentelella/vendors/nprogress/nprogress.css') }} " rel="stylesheet">
+    {{--  <link href="{{ asset('/bower_components/gentelella/vendors/nprogress/nprogress.css') }} " rel="stylesheet">  --}}
     <!-- iCheck -->
-    <link href="{{ asset('/bower_components/gentelella/vendors/iCheck/skins/flat/green.css') }} " rel="stylesheet">
+    {{--  <link href="{{ asset('/bower_components/gentelella/vendors/iCheck/skins/flat/green.css') }} " rel="stylesheet">  --}}
     <!-- bootstrap-progressbar -->
-    <link href="{{ asset('/bower_components/gentelella/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css') }} "
-        rel="stylesheet">
+    {{--  <link href="{{ asset('/bower_components/gentelella/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css') }} " rel="stylesheet">  --}}
     <!-- JQVMap -->
-    <link href="{{ asset('/bower_components/gentelella/vendors/jqvmap/dist/jqvmap.min.css') }} " rel="stylesheet">
+    {{--  <link href="{{ asset('/bower_components/gentelella/vendors/jqvmap/dist/jqvmap.min.css') }} " rel="stylesheet">  --}}
     <!-- bootstrap-daterangepicker -->
-    <link href="{{ asset('/bower_components/gentelella/vendors/bootstrap-daterangepicker/daterangepicker.css') }} " rel="stylesheet">
+    {{--  <link href="{{ asset('/bower_components/gentelella/vendors/bootstrap-daterangepicker/daterangepicker.css') }} " rel="stylesheet">  --}}
     <!-- Custom Theme Style -->
     <link href="{{ asset('/bower_components/gentelella/build/css/custom.min.css') }} " rel="stylesheet">
 
     <!-- LOGISTIC -->
-    <style>
-        .btn {
-            margin-bottom: 0px;
-        }
-        .custom-input {
-            width: 60px;
-        }
-    </style>
+    <link href="{{ asset('/css/logistic.css') }} " rel="stylesheet">
     <link href="{{ asset('/bower_components/angular-bootstrap/ui-bootstrap-csp.css') }} " rel="stylesheet">
     <link href="{{ asset('/bower_components/angular-ui-select/dist/select.css') }} " rel="stylesheet">
     <base href="{{ url('') }}/logistic/">
@@ -74,38 +68,38 @@
     <!-- Bootstrap -->
     <script src="{{ asset('/bower_components/gentelella/vendors/bootstrap/dist/js/bootstrap.min.js') }} "></script>
     <!-- FastClick -->
-    <script src="{{ asset('/bower_components/gentelella/vendors/fastclick/lib/fastclick.js') }} "></script>
+    {{--  <script src="{{ asset('/bower_components/gentelella/vendors/fastclick/lib/fastclick.js') }} "></script>  --}}
     <!-- NProgress -->
-    <script src="{{ asset('/bower_components/gentelella/vendors/nprogress/nprogress.js') }} "></script>
+    {{--  <script src="{{ asset('/bower_components/gentelella/vendors/nprogress/nprogress.js') }} "></script>  --}}
     <!-- Chart.js -->
-    <script src="{{ asset('/bower_components/gentelella/vendors/Chart.js/dist/Chart.min.js') }} "></script>
+    {{--  <script src="{{ asset('/bower_components/gentelella/vendors/Chart.js/dist/Chart.min.js') }} "></script>  --}}
     <!-- gauge.js -->
-    <script src="{{ asset('/bower_components/gentelella/vendors/gauge.js/dist/gauge.min.js') }} "></script>
+    {{--  <script src="{{ asset('/bower_components/gentelella/vendors/gauge.js/dist/gauge.min.js') }} "></script>  --}}
     <!-- bootstrap-progressbar -->
-    <script src="{{ asset('/bower_components/gentelella/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js') }} "></script>
+    {{--  <script src="{{ asset('/bower_components/gentelella/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js') }} "></script>  --}}
     <!-- iCheck -->
-    <script src="{{ asset('/bower_components/gentelella/vendors/iCheck/icheck.min.js') }} "></script>
+    {{--  <script src="{{ asset('/bower_components/gentelella/vendors/iCheck/icheck.min.js') }} "></script>  --}}
     <!-- Skycons -->
-    <script src="{{ asset('/bower_components/gentelella/vendors/skycons/skycons.js') }} "></script>
+    {{--  <script src="{{ asset('/bower_components/gentelella/vendors/skycons/skycons.js') }} "></script>  --}}
     <!-- Flot -->
-    <script src="{{ asset('/bower_components/gentelella/vendors/Flot/jquery.flot.js') }} "></script>
+    {{--  <script src="{{ asset('/bower_components/gentelella/vendors/Flot/jquery.flot.js') }} "></script>
     <script src="{{ asset('/bower_components/gentelella/vendors/Flot/jquery.flot.pie.js') }} "></script>
     <script src="{{ asset('/bower_components/gentelella/vendors/Flot/jquery.flot.time.js') }} "></script>
     <script src="{{ asset('/bower_components/gentelella/vendors/Flot/jquery.flot.stack.js') }} "></script>
-    <script src="{{ asset('/bower_components/gentelella/vendors/Flot/jquery.flot.resize.js') }} "></script>
+    <script src="{{ asset('/bower_components/gentelella/vendors/Flot/jquery.flot.resize.js') }} "></script>  --}}
     <!-- Flot plugins -->
-    <script src="{{ asset('/bower_components/gentelella/vendors/flot.orderbars/js/jquery.flot.orderBars.js') }} "></script>
+    {{--  <script src="{{ asset('/bower_components/gentelella/vendors/flot.orderbars/js/jquery.flot.orderBars.js') }} "></script>
     <script src="{{ asset('/bower_components/gentelella/vendors/flot-spline/js/jquery.flot.spline.min.js') }} "></script>
-    <script src="{{ asset('/bower_components/gentelella/vendors/flot.curvedlines/curvedLines.js') }} "></script>
+    <script src="{{ asset('/bower_components/gentelella/vendors/flot.curvedlines/curvedLines.js') }} "></script>  --}}
     <!-- DateJS -->
-    <script src="{{ asset('/bower_components/gentelella/vendors/DateJS/build/date.js') }} "></script>
+    {{--  <script src="{{ asset('/bower_components/gentelella/vendors/DateJS/build/date.js') }} "></script>  --}}
     <!-- JQVMap -->
-    <script src="{{ asset('/bower_components/gentelella/vendors/jqvmap/dist/jquery.vmap.js') }} "></script>
+    {{--  <script src="{{ asset('/bower_components/gentelella/vendors/jqvmap/dist/jquery.vmap.js') }} "></script>
     <script src="{{ asset('/bower_components/gentelella/vendors/jqvmap/dist/maps/jquery.vmap.world.js') }} "></script>
-    <script src="{{ asset('/bower_components/gentelella/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js') }} "></script>
+    <script src="{{ asset('/bower_components/gentelella/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js') }} "></script>  --}}
     <!-- bootstrap-daterangepicker -->
-    <script src="{{ asset('/bower_components/gentelella/vendors/moment/min/moment.min.js') }} "></script>
-    <script src="{{ asset('/bower_components/gentelella/vendors/bootstrap-daterangepicker/daterangepicker.js') }} "></script>
+    {{--  <script src="{{ asset('/bower_components/gentelella/vendors/moment/min/moment.min.js') }} "></script>
+    <script src="{{ asset('/bower_components/gentelella/vendors/bootstrap-daterangepicker/daterangepicker.js') }} "></script>  --}}
     <!-- Custom Theme Scripts -->
     {{--  <script src="{{ asset('/bower_components/gentelella/build/js/custom.min.js') }} "></script>  --}}
     <script src="{{ asset('/bower_components/gentelella/src/js/helpers/smartresize.js') }} "></script>
--- resources/views/templates/home.php
+++ resources/views/templates/home.php
@@ -15,11 +15,11 @@
 
 
 <div class="row">
-    
+
     <div class="col-sm-2 col-md-2 col-lg-2">
-        
+
     </div>
-    
+
     <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
 
         <div class="panel panel-default">
@@ -27,15 +27,32 @@
 
                 <h1>CLADESYS - LOGISTICS</h1>
                 <h2>
-                    <p>Bienvenido al Sistema de Logistica, este Sistema de Información sirve para registrar productos,
-                        controlar la entrada de estos al almacén, seguir su distribución, controlar el stock de los productos
-                        </p>
-                    <p>- Control de Usuarios (pendiente)</p>
+                    <p>Bienvenido al Sistema de Logistica, este Sistema de Información sirve para administrar varios almcenes,
+                        registrar productos, controlar la entrada de estos a los almacén, seguir su distribución entre estos
+                        almacenes registrados, controlar el stock de los productos
+                    </p>
+                    <!-- <p>- Control de Usuarios (pendiente)</p>
                     <p>- Control de Diferentes ubicaciones </p>
-                    <p>- Proceso de compra, requerimientos, cotización y compra</p>
+                    <p>- Proceso de compra, requerimientos, cotización y compra</p> -->
                 </h2>
             </div>
         </div>
 
+
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <h3>Caracteristicas:</h3>
+                <ul>
+                    <li>Control de usuarios</li>
+                    <li>Control de Stock</li>
+                    <li>Proceso de compra por requerimientos</li>
+                    <li>Configuracion de productos por almacen</li>
+                </ul>
+            </div>
+        </div>
+
+
     </div>
+
+
 </div>

ee3d2b972c3bfdfed3e5569d345a15e30350b0d5
Wilson Pilco Nuñez <wilsonaux1@gmail.com>
2018-01-08 09:22:42 +0000

    listo cotizar con output details

--- database/seeds/SemiCotizar.php
+++ database/seeds/SemiCotizar.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\InputDetails;
+use App\Models\Inputs;
+use App\Models\OutputDetails;
+use App\Models\Outputs;
 
 class SemiCotizar extends Seeder
 {
@@ -32,10 +35,51 @@ class SemiCotizar extends Seeder
                     if(count($list) > 0){
                         // chamba
                         foreach ($list as $key => $value) {
+                            // echo var_dump($data);
+                            print_r($data);
+
                             $value->unit_price = $data[2];
                             $value->save();
-                            echo var_dump($data);
-                            $modificado[] = $value;
+
+                            $input = Inputs::find($value->inputs_id);
+                            $input->observation = ($input->observation ? $input->observation : '') . "
+AutoCotizacion: ($data[0], $value->id) - $data[1]";
+                            $input->save();
+                            
+                            $outs = [];
+
+                            // buscando salidas sin precio
+                            $outdets = OutputDetails::where('input_details_id', $value->id)
+                                ->where('unit_price', '0.000')
+                                ->get();
+
+                            
+
+                            foreach ($outdets as $key => $outdet) {
+                                // registrando cambio en cabecera
+                                $output = Outputs::find($outdet->outputs_id);
+                                $output->observation = ($output->observation ? $output->observation : '') . "
+AutoCotizar: ($data[0], $value->id, $outdet->id) - $data[1]";
+                                $output->save();
+
+                                // registrando cambio en detalle
+                                $outdet->unit_price = $data[2];
+                                $outdet->real_unit_price = $data[2];
+                                $outdet->save();
+
+                                $outs[] = [
+                                    // 'out' => $output->id,
+                                    'dets' => $outdet
+                                ];
+                            }
+
+                            
+                            $modificado[] = [
+                                'input' => $input,
+                                'input_detail' => $value,
+                                'outs' => $outs,
+
+                            ];
                         }
                     }else{
                         $noCotizado[] = [
@@ -56,9 +100,13 @@ class SemiCotizar extends Seeder
         //     $noCotizado
         // );
         // dd();
-        // file_put_contents(
-        //     "./nocotizado.json",
-        //     json_encode(['noCotizados' => $noCotizado])
-        // );
+        file_put_contents(
+            "./nocotizado.json",
+            json_encode([
+                'cotizado' => $modificado,
+                'noCotizados' => $noCotizado
+            ])
+        );
     }
 }
+

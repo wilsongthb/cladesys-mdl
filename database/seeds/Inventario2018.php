877ec007d382796997bcc237d4e7fc28859c3607
Limpp <wilsonaux1@gmail.com>
2018-04-10 20:56:09 -0500

    inventario 2018

--- database/seeds/Inventario2018.php
+++ database/seeds/Inventario2018.php
@@ -21,23 +21,43 @@ class Inventario2018 extends Seeder
         // DB::table('')->insert();
         DB::transaction(function () {
             if (($handle = fopen("./database/seeds/data/ALMACEN_ABRIL_2018.csv", "r")) !== FALSE) {
-                $area_id = DB::table('locations')->insertGetId(['user_id' => 1, 'name' => 'LOGISTICA - 2018']);
-                DB::table([])
-
-
-                $input = new Inputs;
-                $input->user_id = 1;
-                $input->locations_id = $area_id;
-                $input->type = 3;
-                $input->save();
-
-                $input_id = $input->id;
+                $logistica = DB::table('locations')->insertGetId(['user_id' => 1, 'name' => 'LOGISTICA - 2018']);
+                $unidad = DB::table('locations')->insertGetId(['user_id' => 1, 'name' => 'ALMACEN DE UNIDAD - 2018']);
+                $laboratorio = DB::table('locations')->insertGetId(['user_id' => 1, 'name' => 'LABORATORIO - 2018']);
                 
-                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                    $product = DB::table('products')->where('name', 'LIKE', "%$data[1]%")->first();
-                    // print_r($product ? $product->id."\n" : "no encontrado\n");
-                    if(!$product){
+                DB::table('user_locations')->insert([
+                    ['user_id' => 1, 'locations_id' => $logistica],
+                    ['user_id' => 1, 'locations_id' => $unidad],
+                    ['user_id' => 1, 'locations_id' => $laboratorio],
+                ]);
 
+                print_r([$unidad, $logistica, $laboratorio]);
+
+                $input1 = new Inputs;
+                $input1->user_id = 1;
+                $input1->locations_id = $logistica;
+                $input1->type = 3;
+                $input1->save();
+
+                $input2 = new Inputs;
+                $input2->user_id = 1;
+                $input2->locations_id = $unidad;
+                $input2->type = 3;
+                $input2->save();
+
+                $input3 = new Inputs;
+                $input3->user_id = 1;
+                $input3->locations_id = $laboratorio;
+                $input3->type = 3;
+                $input3->save();
+                
+                $product = new stdClass;
+                $product->name = "";
+
+                while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+                    $productl = $product;
+                    $product = DB::table('products')->where('name', 'LIKE', "%$data[1]%")->first();
+                    if(!$product){
                         $product = new Products;
                         $product->user_id = 1;
                         $product->code = 'I2018';
@@ -75,18 +95,92 @@ class Inventario2018 extends Seeder
                         $product->brands_id = $b->id;
                         $product->save();
 
-                        echo "Nuevo Producto: ".$product->name."\n";
+                        echo "Nuevo Producto: ".$product->name.$product->id."\n";
                     }
+                    echo "Existente Producto: ".$product->name.$product->id."\n";
 
-                    if($data[15] !== ""){
-                        $input_details = InputDetails::insertGetId([
-                            'user_id' => 1,
-                            'products_id' => $product->id,
-                            'inputs_id' => $input_id,
-                            'unit_price' => 0,
-                            'quantity' => $data[15]
-                        ]);
+                    if($product->name != $productl->name){
+                        /**INSERTANDO EN UNIDAD */
+                        echo "UNIDAD\n";
+                        $cantMin = 0;
+                        $cantPer = 0;
+                        if($data[5] != ""){
+                            $cantPer = $data[5];
+                            if($data[4] != ""){
+                                $cantMin = $data[4];
+                            }
+                            DB::table('product_options')->insert([
+                                'user_id' => 1,
+                                'minimum' => $cantMin,
+                                'permanent' => $cantPer,
+                                'products_id' => $product->id,
+                                'locations_id' => $unidad
+                            ]);
+                            DB::table('input_details')->insert([
+                                'user_id' => 1,
+                                'products_id' => $product->id,
+                                'inputs_id' => $input2->id,
+                                'unit_price' => 0,
+                                'quantity' => $cantPer ? $cantPer : $cantMin
+                            ]);
+                        }
+
+                        /**INSERTAR A LABORATORIO */
+                        echo "LABORATORIO\n";
+                        $cantMin = 0;
+                        $cantPer = 0;
+                        if($data[9] != ""){
+                            $cantPer = $data[9];
+                            if($data[8] != ""){
+                                $cantMin = $data[8];
+                            }
+                            DB::table('product_options')->insert([
+                                'user_id' => 1,
+                                'minimum' => $cantMin,
+                                'permanent' => $cantPer,
+                                'products_id' => $product->id,
+                                'locations_id' => $laboratorio
+                            ]);
+                            DB::table('input_details')->insert([
+                                'user_id' => 1,
+                                'products_id' => $product->id,
+                                'inputs_id' => $input3->id,
+                                'unit_price' => 0,
+                                'quantity' => $cantPer ? $cantPer : $cantMin
+                            ]);
+                        }
+
+
+                        /**INSERTAR A LOGISTICA */
+                        echo "LOGISTICA\n";
+                        $cantMin = 0;
+                        $cantPer = 0;
+                        if($data[9] != ""){
+                            $cantPer = $data[9];
+                            if($data[8] != ""){
+                                $cantMin = $data[8];
+                            }
+                            DB::table('product_options')->insert([
+                                'user_id' => 1,
+                                'minimum' => $cantMin,
+                                'permanent' => $cantPer,
+                                'products_id' => $product->id,
+                                'locations_id' => $logistica
+                            ]);
+                            
+                        }
+
+                        if($data[15] !== ""){
+                            DB::table('input_details')->insert([
+                                'user_id' => 1,
+                                'products_id' => $product->id,
+                                'inputs_id' => $input1->id,
+                                'unit_price' => 0,
+                                'quantity' => $data[15]
+                            ]);
+                        }
                     }
+                    
                 }
             }
         });
